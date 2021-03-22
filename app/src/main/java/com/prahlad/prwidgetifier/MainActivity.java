29ed3e3ff9dde9b165fd4db253cedc520fd2276f
prahladinala <prahlad2018@gmail.com>
2021-03-22 21:19:20 +0530

    initial commit

---
+++ app/src/main/java/com/prahlad/prwidgetifier/MainActivity.java
@@ -0,0 +1,21 @@
+package com.prahlad.prwidgetifier;
+
+import android.content.Context;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button P, PR, PRA, PRAH, PRAHL, PRAHLA, PRAHLAD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/prahlad/prwidgetifier/SeventhWidget.java
@@ -0,0 +1,52 @@
+package com.prahlad.prwidgetifier;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link SeventhWidgetConfigureActivity SeventhWidgetConfigureActivity}
+ */
+public class SeventhWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = SeventhWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.seventh_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (int appWidgetId : appWidgetIds) {
+            SeventhWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
